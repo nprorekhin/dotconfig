55768c843dbc5438492f519323beee1afd904ccb
Nikita Prorekhin <nikita.prorekhin+github@gmail.com>
2026-02-24 21:13:46 +0100

    feat: options for nvim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,8 +1,18 @@
-vim.opt.number = true
-vim.opt.cursorline = true
-vim.opt.relativenumber = true
-vim.opt.shiftwidth = 4
+-- Print the line number in front of each line
+vim.o.number = true
 
+-- Use relative line numbers, so that it is easier to jump with j, k. This will affect the 'number'
+-- option above, see `:h number_relativenumber`
+vim.o.relativenumber = true
 
+-- Highlight the line where the cursor is on
+vim.o.cursorline = true
 
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 10
+
+vim.o.shiftwidth = 2
