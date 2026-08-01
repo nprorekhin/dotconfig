d2db48736fe6e76eb7e016d112c9c26820ace1eb
Nikita Prorekhin <nikita.prorekhin+github.com>
2026-08-01 10:34:40 +0200

    chore(nvim): no plugins vim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,25 @@
-require('config.options')
-require('config.keybinds')
-require('config.lazy')
+-- leader key mapping
+vim.g.mapleader = " " 			--space is a leader key
+vim.g.maplocalleader = " "
+
+-- options
+local opt = vim.opt
+opt.number = true
+opt.relativenumber = true
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.smartindent = true
+opt.wrap = false
+opt.ignorecase = true
+opt.smartcase = true
+opt.scrolloff = 7
+opt.clipboard = "unnamedplus"
+opt.undofile = true
+opt.splitright = tue
+opt.splitbelow = true
+
+-- mappings
+vim.keymap.set("n", "<leader>w", "<cmd>write<cr>", { desc = "Save" })
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<cr>", { desc = "Clear search hightlight" })
+
